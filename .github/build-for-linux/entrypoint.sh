94f079a94b30fe1e12a71443cb512a166fec2265
huzibaca <masterorigin91@gmail.com>
2024-09-06 04:57:59 +0800

    chore： update

--- .github/build-for-linux/entrypoint.sh
+++ .github/build-for-linux/entrypoint.sh
@@ -10,7 +10,9 @@ rustup target add "$INPUT_TARGET"
 if [ "$INPUT_TARGET" = "x86_64-unknown-linux-gnu" ]; then
     add-apt-repository universe
     apt-get update 
-    apt-get install -y build-essential curl wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev libgtk-3-dev
+    apt-get install -y build-essential curl wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev libglib2.0-dev
+    find /usr -name "glib-2.0.pc"
+    pkg-config --libs --cflags glib-2.0
     export PKG_CONFIG_PATH=/usr/share/pkgconfig:/usr/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH
 elif [ "$INPUT_TARGET" = "i686-unknown-linux-gnu" ]; then
     dpkg --add-architecture i386
